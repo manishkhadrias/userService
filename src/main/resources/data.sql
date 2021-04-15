b7062e337561d0f98d4baec0b5308960e43153d1
Manish Khadria <manish.khadria@nagarro.com>
2021-04-15 10:51:44 +0530

    clusterIp

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+delete from user where id in (1,2);
 INSERT INTO user (id,name,age,email,host) VALUES (1,'Manish','33','manish.khadria@nagarro.com','data.sql');
 INSERT INTO user (id,name,age,email,host) VALUES (2,'John','23','john.doe@google.com','data.sql');
--- src/test/java/com/mk/user/UserApplicationTests.java
+++ src/test/java/com/mk/user/UserApplicationTests.java
@@ -2,6 +2,7 @@ package com.mk.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
 class UserApplicationTests {
